1e665d62e8db19dc57860c3283b9f11ce7b5f781
firetdev <futuregamesstore@gmail.com>
2026-05-11 22:34:49 -0500

    Start project

--- Elasticity/main.cpp
+++ Elasticity/main.cpp
@@ -1,14 +1,25 @@
-//
-//  main.cpp
-//  Elasticity
-//
-//  Created by Yeshaya Zeff on 5/11/26.
-//
-
+#include <SFML/Graphics.hpp>
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return EXIT_SUCCESS;
+int main() {
+    sf::RenderWindow window(sf::VideoMode({800, 600}), "Elasticity", sf::Style::Titlebar | sf::Style::Close);
+    sf::CircleShape shape(15.f);
+    shape.setFillColor(sf::Color::Green);
+    
+    sf::Font font;
+
+    if (!font.openFromFile("Anonymous_Pro 2.ttf"))
+        std::cout << "Failed to load font\n";
+
+
+    while (window.isOpen()) {
+        while (const std::optional event = window.pollEvent()) {
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
 }
