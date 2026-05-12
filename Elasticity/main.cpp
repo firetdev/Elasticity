dbf0d970550bd65a42466f80c691cb5f30c6e869
firetdev <futuregamesstore@gmail.com>
2026-05-11 22:18:42 -0500

    Initial Commit

---
+++ Elasticity/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Elasticity
+//
+//  Created by Yeshaya Zeff on 5/11/26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
